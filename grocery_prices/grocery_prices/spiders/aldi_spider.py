3cf2960ad865639a3a5eafbd59ca1435737745e7
Jai <jsarava2@ncsu.edu>
2024-08-26 01:08:17 -0400

    aldi spider edits

--- grocery_prices/grocery_prices/spiders/aldi_spider.py
+++ grocery_prices/grocery_prices/spiders/aldi_spider.py
@@ -33,7 +33,8 @@ class AldiSpider(scrapy.Spider):
 
         sel = Selector(text=self.driver.page_source)
 
-        products = sel.css('a.base-link.product-tile__content.product-tile__link')
+        # Correct selector for the product tiles
+        products = sel.css('div.product-tile')
         logging.info(f"Found {len(products)} products")
 
         for product in products:
@@ -47,12 +48,12 @@ class AldiSpider(scrapy.Spider):
             logging.info(f"Found product name: {item_name}")
 
             # Extract original price
-            original_price = product.css('span.base-price__regular span:first-child::text').get()
+            original_price = product.css('span.base-price__regular span::text').get()
             item['price'] = original_price.strip('$') if original_price else 'N/A'
 
             # Extract price per unit
-            price_per_unit = product.css('div.product-detail::text').get()
-            item['price_per_unit'] = price_per_unit.strip('()').strip() if price_per_unit else 'N/A'
+            price_per_unit = product.css('div.product-tile__unit-of-measurement p::text').get()
+            item['price_per_unit'] = price_per_unit.strip() if price_per_unit else 'N/A'
 
             # Default sale price to 'N/A' since no sale indicator is given
             item['sale_price'] = 'N/A'
@@ -62,9 +63,6 @@ class AldiSpider(scrapy.Spider):
             logging.info(f"Scraped item: {item}")
             yield item
 
-        # Close the current browser window
-        self.driver.quit()
-
         # Handle pagination if there are more pages to scrape
         current_page = response.meta['page']
         total_pages = response.meta['total_pages']
@@ -73,9 +71,6 @@ class AldiSpider(scrapy.Spider):
 
         # Check if there are more pages to scrape
         if initial or next_page <= total_pages:
-            # # Introduce a delay before making the next request
-            # time.sleep(300)
-            
             # Reinitialize the driver
             self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
